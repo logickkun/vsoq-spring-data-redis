5bdf368348fbc1eff80ddbb105047c16c36c9792
logickkun <c.hyunmyung@gmail.com>
2025-09-07 00:41:08 +0900

    1. bff 레이어에 세션 저장 추가 2. 로그인 요청 시 -> authN으로 먼저 요청하고 -> authZ 프론트로 전달하는 프로세스로 변경 3.

---
+++ src/test/java/com/logickkun/vsoq/spring/data/redis/SpringDataRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logickkun.vsoq.spring.data.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
